639c867c43019f8b361a3601378441a9fd90c139
Andrea Nerla <andreanerla96@gmail.com>
2022-07-03 18:03:31 +0200

    completato il test di 1d

--- exercises/src/test/scala/exercises/valfunction/ValueFunctionExercisesTest.scala
+++ exercises/src/test/scala/exercises/valfunction/ValueFunctionExercisesTest.scala
@@ -23,10 +23,59 @@ class ValueFunctionExercisesTest extends AnyFunSuite with ScalaCheckDrivenProper
     }
   }
 
+  // checks if all chars returns null
+  test("selectDigits is null") {
+    assert(selectDigits("text").length == 0)
+    }
+  
+
+  // checks that every output char is digit
+  test("Every output elem is digit ") {
+    selectDigits("text").foreach(c => assert(c.isDigit))
+    }
+  
+
+
+  //secret returns same length
+  test("secret returns same length") {
+    secret("text").length == ("text").length
+    }
+  
+
+  //every char after secret is asterisk
+  test("every char after secret is asterisk") {
+    secret("text").foreach(c => assert(c == '*'))
+    }
+  
+
+  test("secret is idempotent") {
+    secret("text").foreach(c => assert(c == '*'))
+  val secret_once = secret("text")  
+  val secret_twice = secret(secret("text"))
+  assert(secret_once == secret_twice)
+  }
+
+  test("isValidUsernameCharacter returns alse if '^' ") {
+    assert(isValidUsernameCharacter('^') == false)  
+  }
+  
+  test("isValidUsername returns true if john-doe ") {
+    isValidUsername("john-doe") == true  
+  }
+
+  test("isValidUsername returns false if john*doe ") {
+    forAll{(username: String) =>
+    isValidUsername(username.reverse) == isValidUsername(username)  
+    }
+  }
+
+
+
+}
 
   ///////////////////////
   // Exercise 2: Point
   ///////////////////////
 
-}
+
 
